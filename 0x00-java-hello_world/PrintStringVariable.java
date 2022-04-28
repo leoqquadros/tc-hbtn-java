e192d63b0cf36af99c889d30efd7f50b2bca3b48
Leonardo Quadros <leonardoq@ciandt.com>
2022-04-28 10:13:24 -0300

    novo

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,7 @@
+public class PrintStringVariable {
+    public static void main(String[] args) {
+        String texto = "Holberton School";
+        System.out.printf("%s\n%s\n%.9s\n", texto, texto, texto);
+    }
+
+}
