0eaffdf0bc53eeda73efd505fb384c55437146c5
Leonardo Quadros <leonardoq@ciandt.com>
2022-04-28 09:10:54 -0300

    novo

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,8 @@
+public class PrintIntegerVariable {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
+
+
--- 0x00-java-hello_world/Println.java
+++
@@ -1,5 +0,0 @@
-public class Printl {
-	    public static void main(String[] args) {
-		            System.out.println("Holberton School");
-			        }
-}
