cbcefb845f3071cf0a3e628b18f716f8e9d71116
Leonardo Quadros <leonardoq@ciandt.com>
2022-04-28 10:29:31 -0300

    novo

---
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -0,0 +1,8 @@
+public class PrintFormatacaoNumeros {
+    public static void main(String[] args) {
+        float taxa = 0.2456f;
+        float valor = 7654.321f;
+        System.out.printf("Valor: %.2f\nTaxa: %.2f%%\n", valor, taxa);
+
+    }
+}
