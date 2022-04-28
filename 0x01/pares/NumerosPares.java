8f191062c391ae193475c31fd51f4a9903a63515
Leonardo Quadros <leonardoq@ciandt.com>
2022-04-28 16:58:10 -0300

    novo

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,11 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        for (int i=0; i<100; i++){
+            if ( i %2 == 0) {
+                if (i != 98)
+                System.out.printf("%d, ", i);
+                else System.out.printf("%d\n ", i);
+            }
+        }
+    }
+}
