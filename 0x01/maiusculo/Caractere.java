adf4eabce2a830958b6ec90e53c9dbc137a040b5
Leonardo Quadros <leonardoq@ciandt.com>
2022-04-29 11:22:17 -0300

    novo

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,11 @@
+package maiusculo;
+public class Caractere {
+    public static boolean EhMaiusculo(char c){
+        if ((c >= 'A') && (c <= 'Z')){
+            return true;
+        }else {
+            return false;
+        }
+     }
+
+}
