56dc49d21acc43ebd08c64226448a57335e8a46c
Leonardo Quadros <leonardoq@ciandt.com>
2022-05-03 16:04:42 -0300

    Exceção

--- 0x03/conta_basica/exceptions/OperacaoInvalidaException.java
+++ 0x03/conta_basica/exceptions/OperacaoInvalidaException.java
@@ -1,8 +1,7 @@
 package exceptions;
 public class OperacaoInvalidaException extends Exception{
-
-    private static final long serialVersionUID = 1L;
     public OperacaoInvalidaException (String msg) {
+
         super (msg);
     }
 }
