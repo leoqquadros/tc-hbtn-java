676c5957c1bbe2122890d2f5ffad39f488aaa885
Leonardo Quadros <leonardoq@ciandt.com>
2022-05-05 09:36:53 -0300

    Exceção

--- 0x05/forma/Program.java
+++ 0x05/forma/Program.java
@@ -1,11 +1,11 @@
 public class Program {
-    FormaGeometrica formaGeometrica = new FormaGeometrica();
+    public static void main(String[] args) {
+        FormaGeometrica formaGeometrica = new FormaGeometrica();
 
-            try {
-        System.out.printf("Area: %d", formaGeometrica.area());
-    } catch (Exception e) {
-        System.out.println(e);
+        try {
+            System.out.printf("Area: %d", formaGeometrica.area());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-}
-
 }
