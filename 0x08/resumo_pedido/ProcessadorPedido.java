db16d38c1301bc7dc8ce4a7c8bfb5153d41499c2
Leonardo Quadros <leonardoq@ciandt.com>
2022-05-09 15:56:15 -0300

    Exceção

--- 0x08/resumo_pedido/ProcessadorPedido.java
+++ 0x08/resumo_pedido/ProcessadorPedido.java
@@ -1,15 +1,11 @@
 import provedores.ProvedorFrete;
 
-private class ProcessadorPedido {
+public class ProcessadorPedido {
 
     private ProvedorFrete provedorFrete;
     private Frete frete = new Frete();
 
-    public ProcessadorPedido() {
-
-    }
     public ProcessadorPedido(ProvedorFrete provedorFrete) {
-        super();
         this.provedorFrete = provedorFrete;
     }
     public void processar(Pedido pedido) {
