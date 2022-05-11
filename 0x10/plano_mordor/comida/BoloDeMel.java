8d8dae5349f189c9751d9e7b51376bc4be3a9f35
Leonardo Quadros <leonardoq@ciandt.com>
2022-05-11 17:01:35 -0300

    Exceção

---
+++ 0x10/plano_mordor/comida/BoloDeMel.java
@@ -0,0 +1,11 @@
+package comida;
+
+public class BoloDeMel extends Comida{
+
+    public BoloDeMel() {
+        pontosDeFelicidade = 5;
+    }
+    public BoloDeMel(int pontosDeFelicidade) {
+        super(pontosDeFelicidade);
+    }
+}
