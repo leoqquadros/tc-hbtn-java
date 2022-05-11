413c01997eaa02a8dca240355324491df68a94c7
Leonardo Quadros <leonardoq@ciandt.com>
2022-05-11 16:44:23 -0300

    Exceção

---
+++ 0x10/comida/BolodeMel.java
@@ -0,0 +1,11 @@
+package comida;
+
+public class BolodeMel extends Comida{
+
+    public BolodeMel() {
+        pontosDeFelicidade = 5;
+    }
+    public BolodeMel(int pontosDeFelicidade) {
+        super(pontosDeFelicidade);
+    }
+}
---
+++ 0x10/comida/Cram.java
@@ -0,0 +1,11 @@
+package comida;
+
+public class Cram extends Comida{
+    public Cram(int pontosDeFelicidade) {
+        super(pontosDeFelicidade);
+    }
+
+    public Cram(){
+        pontosDeFelicidade = 2;
+    }
+}
