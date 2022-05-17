16864a1997bada3363cb01824adc357f2699f013
Leonardo Quadros <leonardoq@ciandt.com>
2022-05-17 17:49:33 -0300

    Collections

---
+++ Set_Map/telefones/ListaTelefonica.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class ListaTelefonica {
+
+    private HashMap<String, ArrayList<Telefone>> listaTelefones;
+
+    public ListaTelefonica() {
+        listaTelefones = new HashMap<>();
+    }
+
+    public void adicionarTelefone(String nome, Telefone telefone) {
+        ArrayList<Telefone> telTemp = new ArrayList<Telefone>();
+
+        if (!listaTelefones.containsKey(nome)) {
+            telTemp.add(telefone);
+            listaTelefones.put(nome, telTemp);
+        } else {
+            telTemp = listaTelefones.get(nome);
+            telTemp.add(telefone);
+            listaTelefones.put(nome, telTemp);
+        }
+    }
+    public ArrayList<Telefone> buscar(String nome) {
+
+        ArrayList<Telefone> telTemp = new ArrayList<Telefone>();
+
+        if (!listaTelefones.containsKey(nome)) {
+            return null;
+        } else {
+            telTemp = listaTelefones.get(nome);
+            return telTemp;
+        }
+    }
+}
