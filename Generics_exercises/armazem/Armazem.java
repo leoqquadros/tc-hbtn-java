c68342070b23c0f25a750b0cc8430f64a9bb2e97
Leonardo Quadros <leonardoq@ciandt.com>
2022-05-23 14:09:08 -0300

    Collections

---
+++ Generics_exercises/armazem/Armazem.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class Armazem<T> implements Armazenavel<T> {
+
+    private Map<String, T> mapa = new TreeMap<>();
+
+
+    @Override
+    public void adicionarAoInventario(String nome, T valor) {
+        mapa.put(nome, valor);
+
+    }
+
+    @Override
+    public T obterDoInventario(String nome) {
+
+
+        return mapa.get(nome);
+    }
+
+}
