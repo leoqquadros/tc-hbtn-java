ba4d60f63738c016e0821701cc50a0a51df52e8c
Leonardo Quadros <leonardoq@ciandt.com>
2022-05-25 17:58:55 -0300

    Lambda

---
+++ Lambda/liquido/FolhaPagamento.java
@@ -0,0 +1,6 @@
+public class FolhaPagamento {
+    public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculadora) {
+        return calculadora.calcular(funcionario.getSalario());
+    }
+
+}
