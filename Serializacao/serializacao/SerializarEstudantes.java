945508af25bb6b2542196bbe0379b29137ca2a82
Leonardo Quadros <leonardoq@ciandt.com>
2022-05-31 16:57:57 -0300

    serializacao

--- Serializacao/serializacao/SerializarEstudantes.java
+++ Serializacao/serializacao/SerializarEstudantes.java
@@ -1,9 +1,5 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+
+import java.io.*;
 import java.util.List;
 
 public class SerializarEstudantes<T> {
